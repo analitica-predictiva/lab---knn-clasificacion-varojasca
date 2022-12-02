eaa36b4b4142a3dbf30e0c89acce3d6c636d76b0
Valeria Rojas <varojasca@unal.edu.co>
2022-12-01 23:06:50 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -16,29 +16,28 @@ def pregunta_01():
     df = pd.read_csv("house-votes-84.csv", sep=",")
 
     # Cree un vector con la variable de respuesta ('party')
-    y = df[____].____
+    y = df['party'].copy()
 
     # Extraiga las variables de entrada
-    X = df.drop(____, _____=1).values
+    X = df.drop('party', axis=1).values
 
     # Importe el transformador OrdinalEncoder
-    from ____ import ____
+    from sklearn.preprocessing import OrdinalEncoder
 
     # Transforme las variables de entrada usando fit_transform
-    X = ____().____(____)
+    X = OrdinalEncoder().fit_transform(X)
 
     # Importe KNeighborsClassifier de sklearn.neighbors
-    from ____ import ____
+    from sklearn.neighbors import KNeighborsClassifier
     
-
     # Cree un un clasificador k-NN con 6 vecinos
-    knn = ____(____=____)
+    knn = KNeighborsClassifier(n_neighbors=6)
 
     # Entrene el clasificador con el conjunto de entrenamiento
-    knn.____(____, ____)
+    knn.fit(X, y)
 
     # Retorne el score del clasificador
-    return knn.____(____, ____)
+    return knn.score(X, y)
 
 
 def pregunta_02():
