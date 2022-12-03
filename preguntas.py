f7a66656fa837b8c2f045c6101ec07a060981abd
Valeria Rojas <111819354+varojasca@users.noreply.github.com>
2022-12-02 21:57:36 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -64,7 +64,7 @@ def pregunta_02():
     from sklearn.neighbors import KNeighborsClassifier
     
     # Cree un un clasificador k-NN con 6 vecinos
-    knn = KNeighborsClassifier(n_neighbors=6)
+    knn = KNeighborsClassifier(n_neighbors=5)
 
     # Entrene el clasificador con el conjunto de entrenamiento
     knn.fit(X, y)
